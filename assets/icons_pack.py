b67b89797ee15ca63276ef7e870212175ab118af
itchimi2p2qa <itchimi.2p2qa@passmail.com>
2026-08-23 19:50:27 +0800

    Add bundled sticker pack so icons work offline

---
+++ assets/icons_pack.py
@@ -0,0 +1,11 @@
+# Bundled Twemoji 14.0.2 72x72 PNGs. CC-BY 4.0 Twitter, Inc.
+from icons_pack_0 import ICONS_PNG as _0
+from icons_pack_1 import ICONS_PNG as _1
+from icons_pack_2 import ICONS_PNG as _2
+from icons_pack_3 import ICONS_PNG as _3
+
+ICONS_PNG = {}
+ICONS_PNG.update(_0)
+ICONS_PNG.update(_1)
+ICONS_PNG.update(_2)
+ICONS_PNG.update(_3)
