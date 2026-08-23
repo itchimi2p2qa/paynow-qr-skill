dd19855ec334eb20a20350b346d0ed35cf1f652b
itchimi2p2qa <itchimi.2p2qa@passmail.com>
2026-08-23 19:51:19 +0800

    Merge eight bundled sticker shards

--- assets/icons_pack.py
+++ assets/icons_pack.py
@@ -3,9 +3,11 @@ from icons_pack_0 import ICONS_PNG as _0
 from icons_pack_1 import ICONS_PNG as _1
 from icons_pack_2 import ICONS_PNG as _2
 from icons_pack_3 import ICONS_PNG as _3
+from icons_pack_4 import ICONS_PNG as _4
+from icons_pack_5 import ICONS_PNG as _5
+from icons_pack_6 import ICONS_PNG as _6
+from icons_pack_7 import ICONS_PNG as _7
 
 ICONS_PNG = {}
-ICONS_PNG.update(_0)
-ICONS_PNG.update(_1)
-ICONS_PNG.update(_2)
-ICONS_PNG.update(_3)
+for part in (_0, _1, _2, _3, _4, _5, _6, _7):
+    ICONS_PNG.update(part)
